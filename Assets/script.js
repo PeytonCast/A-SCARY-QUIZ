94108ee95642dd2f0faf6a836d630a9dee7c7a53
PeytonCast <107663364+PeytonCast@users.noreply.github.com>
2022-07-26 17:21:18 -0400

    Add files via upload
    Over the weekend I was working on the quiz website, and this is the design and structure I went for. 
    So, in this commit, I add the files which is the bulk of the site (HTML and CSS). Now comes the hard part JavaScript.

---
+++ Assets/script.js
@@ -0,0 +1,18 @@
+//ok so we will need var that are set to the id and classes of the HTML
+
+//we will need a landing page with a start button
+//when the start button is pressed the start button will be disabled or disapear
+//and start the quiz
+
+//when the start button is pressed the TIMER will start
+//the timer will need to be five min long
+//IF the user gets a queston wrong 
+//THEN the timer will decrease by 30 sec
+
+//next to the timer there will be a queston i will need the question to remove itself and the display the next question
+//there will be a frationated progress counter that will display the users progress exp 1 of 3
+
+//when the user is finished the user will be given a score 
+//the user can then SAVE the score and his or her name in local storage
+//if one of the 2 values are empty then alert them that the user must fill the name 
+//then the user will be given a requiz button that will restart the quiz
