b43f534c03736943180712c63359b625b046aa51
PeytonCast <107663364+PeytonCast@users.noreply.github.com>
2022-07-31 00:27:47 -0400

    Update score.js
    In this commit I FINELY got this code to save the name and the score!

--- Assets/score.js
+++ Assets/score.js
@@ -1,30 +1,33 @@
+//list of vars
 var me = document.getElementById('me')
-var value1 = localStorage.getItem('value1')
-var value2 = localStorage.getItem('value2')
-var value3 = localStorage.getItem('value3')
-var scoreOf  = document.createElement("p");
-var playerName  = document.createElement("inputName");
-console.log('1', value1)
-console.log('2', value2)
-console.log('3', value3)
-//function clearScore() {
-    //newItem.hidden
-    //newItem.textContent = ``
-    //me.removeChild(newItem)
-//}
+var inputSaveName = document.getElementById('others')
+var otherScores = document.createElement('p');
+var value = localStorage.getItem('value')
+var scoreOf  = document.createElement('p');
+var storageInput = document.getElementById('storage')
+var saveBtn = document.getElementById('save')
+var tryAgain = document.getElementById('tryAgain')
+// allows user to input a name and display it with his or her score
+storageInput.addEventListener('input', name=>{
+    inputSaveName.textContent = name.target.value + ` got ${value} of 3`
+})
+var save = () => {
+    localStorage.setItem('list', inputSaveName.textContent) 
+}
+saveBtn.addEventListener('click', save)
+
+// for displaying the results
 function largestValue() {
-    if (value3 > value2) {
-        scoreOf.textContent = `3 of 3`
-    } else if(value2 > value1) {scoreOf .textContent = `2 of 3`
-    }else if (value1 === 0){scoreOf .textContent = `1 of 3`}else{`0 of 3`}
+    scoreOf.textContent = `${value} of 3`
     
 }
-//clearScore()
-largestValue()
-me.appendChild(scoreOf )
 
-console.log(storeResults)
-//i need to add the data of the score from local storage into an array then convert that array into a LIST containing the name of the player
-// then the score and display that list on index3.html
- localStorage.getItem('correctQ1')
+// displays imedate results 
+largestValue()
+me.appendChild(scoreOf)
+// saves as a html el
+inputSaveName.appendChild(otherScores)
+otherScores.textContent = localStorage.getItem('list')
+
+
 
